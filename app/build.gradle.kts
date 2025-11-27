e6ff1189e1b27ebdc0047240a38ba824ec2ad506
Thiago Toazza <thiago.toazza@townsq.com.br>
2025-11-26 23:30:13 -0300

    Redis setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,5 +16,6 @@ dependencies {
 	implementation(libs.spring.boot.starter.security)
 
 	implementation(libs.spring.boot.starter.data.jpa)
+	implementation(libs.spring.boot.starter.data.redis)
 	runtimeOnly(libs.postgresql)
 }
