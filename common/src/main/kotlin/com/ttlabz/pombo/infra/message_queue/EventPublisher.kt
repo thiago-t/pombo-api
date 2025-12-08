600ad6f2a8b4e8aa22e2a61015536fda90f26d1d
Thiago Toazza <thiago.toazza@townsq.com.br>
2025-12-08 20:52:22 -0300

    Build Event Publisher

---
+++ common/src/main/kotlin/com/ttlabz/pombo/infra/message_queue/EventPublisher.kt
@@ -0,0 +1,26 @@
+package com.ttlabz.pombo.infra.message_queue
+
+import com.ttlabz.pombo.domain.events.ChirpEvent
+import org.slf4j.LoggerFactory
+import org.springframework.amqp.rabbit.core.RabbitTemplate
+
+class EventPublisher(
+    private val rabbitTemplate: RabbitTemplate
+) {
+
+    private val logger = LoggerFactory.getLogger(javaClass)
+
+    fun <T: ChirpEvent> publish(event: T) {
+        try {
+            rabbitTemplate.convertAndSend(
+                event.exchange,
+                event.eventKey,
+                event
+            )
+            logger.info("Successfully published event: ${event.eventKey}")
+        } catch (e: Exception) {
+            logger.error("Failed to publish ${event.eventKey} event", e)
+        }
+    }
+
+}
