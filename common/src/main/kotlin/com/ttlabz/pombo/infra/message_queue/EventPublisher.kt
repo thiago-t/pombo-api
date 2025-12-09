0bb6bbbd8a24e16549013a978f33f5947e46951e
Thiago Toazza <thiago.toazza@townsq.com.br>
2025-12-08 21:02:46 -0300

    Added missing @Component annotation

--- common/src/main/kotlin/com/ttlabz/pombo/infra/message_queue/EventPublisher.kt
+++ common/src/main/kotlin/com/ttlabz/pombo/infra/message_queue/EventPublisher.kt
@@ -3,7 +3,9 @@ package com.ttlabz.pombo.infra.message_queue
 import com.ttlabz.pombo.domain.events.ChirpEvent
 import org.slf4j.LoggerFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
+import org.springframework.stereotype.Component
 
+@Component
 class EventPublisher(
     private val rabbitTemplate: RabbitTemplate
 ) {
