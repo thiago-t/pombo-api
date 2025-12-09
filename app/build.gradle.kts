f2971ee2c8b79828243016faa2d6a718fbf2ffaa
Thiago Toazza <thiago.toazza@townsq.com.br>
2025-12-09 20:47:15 -0300

    Mailgun Setup

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,6 +15,7 @@ dependencies {
 	implementation(libs.kotlin.reflect)
 	implementation(libs.spring.boot.starter.security)
 
+	implementation(libs.spring.boot.starter.mail)
 	implementation(libs.spring.boot.starter.amqp)
 	implementation(libs.spring.boot.starter.data.jpa)
 	implementation(libs.spring.boot.starter.data.redis)
