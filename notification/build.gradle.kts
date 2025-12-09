ba67686fd179c3511b60ca5bbd20ba0da1d804c6
Thiago Toazza <thiago.toazza@townsq.com.br>
2025-12-08 21:51:33 -0300

    Email template service

--- notification/build.gradle.kts
+++ notification/build.gradle.kts
@@ -17,6 +17,7 @@ dependencies {
     implementation(projects.common)
 
     implementation(libs.spring.boot.starter.amqp)
+    implementation(libs.spring.boot.starter.thymeleaf)
 
     testImplementation(kotlin("test"))
 }
---
+++ notification/src/main/kotlin/com/ttlabz/pombo/service/EmailTemplateService.kt
@@ -0,0 +1,25 @@
+package com.ttlabz.pombo.service
+
+import org.springframework.stereotype.Service
+import org.thymeleaf.TemplateEngine
+import org.thymeleaf.context.Context
+
+@Service
+class EmailTemplateService(
+    private val templateEngine: TemplateEngine,
+) {
+
+    fun processTemplate(
+        templateName: String,
+        variables: Map<String, Any> = emptyMap()
+    ): String {
+        val context = Context().apply {
+            variables.forEach { (key, value) ->
+                setVariable(key, value)
+            }
+        }
+
+        return templateEngine.process(templateName, context)
+    }
+
+}
