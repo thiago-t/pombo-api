7f97e5e1e64bb739bbd8a1b37ad03f46cbf4f643
Thiago Toazza <thiago.toazza@townsq.com.br>
2025-12-10 20:54:33 -0300

    Chat Participants Repository

---
+++ chat/src/main/kotlin/com/ttlabz/pombo/chat/infra/database/repositories/ChatParticipantRepository.kt
@@ -0,0 +1,17 @@
+package com.ttlabz.pombo.chat.infra.database.repositories
+
+import com.ttlabz.pombo.chat.infra.database.entities.ChatParticipantEntity
+import com.ttlabz.pombo.domain.type.UserId
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+
+interface ChatParticipantRepository : JpaRepository<ChatParticipantEntity, UserId> {
+    fun findByUserIdIn(userIds: List<UserId>): Set<ChatParticipantEntity>
+
+    @Query("""
+        SELECT p
+        FROM ChatParticipantEntity p
+        WHERE LOWER(p.username) = :query OR LOWER(p.email) = :query 
+    """)
+    fun findByEmailOrUsername(query: String): ChatParticipantEntity?
+}
