020a1972494fa2a8c7eb234e8bfa8e67a60424bf
Thiago Toazza <thiago.toazza@townsq.com.br>
2026-03-13 19:52:47 -0300

    Return 403 For Unverified Emails

--- user/src/main/kotlin/com/ttlabz/pombo/api/exception_handling/AuthExceptionHandler.kt
+++ user/src/main/kotlin/com/ttlabz/pombo/api/exception_handling/AuthExceptionHandler.kt
@@ -50,7 +50,7 @@ class AuthExceptionHandler {
     )
 
     @ExceptionHandler(EmailNotVerifiedException::class)
-    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
     fun onEmailNotVerified(
         e: EmailNotVerifiedException
     ) = mapOf(
